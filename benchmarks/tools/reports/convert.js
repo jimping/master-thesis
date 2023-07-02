3b30db665af7990c87972b3be9974f5ca25120fd
Jonas Imping <j.imping@splintnet.de>
2023-07-02 14:27:47 +0200

    percent fixes

--- benchmarks/tools/reports/convert.js
+++ benchmarks/tools/reports/convert.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
+const util = require('util');
 const path = require('path');
 const fs = require('fs');
+const exec = util.promisify(require('child_process').exec);
 
 module.exports = async () => {
     const files = fs.readdirSync(path.join(__dirname, '../../reports'))
@@ -16,16 +18,22 @@ module.exports = async () => {
     });
 
     for (let file of files) {
+        const pdfFile = file.path.replace('.html', '.pdf');
+        const pngFile = file.path.replace('.html', '.png');
+
         const page = await browser.newPage();
         await page.setContent(file.content, {waitUntil: 'domcontentloaded'});
         await page.pdf({
             scale: 0.85,
-            path: file.path.replace('.html', '.pdf'),
+            path: pdfFile,
             margin: {top: '0px', right: '10px', bottom: '0px', left: '10px'},
             printBackground: true,
             format: 'A4',
         });
         await page.close();
+
+        // convert to png for doc file
+        await exec(`convert -density 300 ${pdfFile} ${pngFile}`)
     }
 
     await browser.close();
